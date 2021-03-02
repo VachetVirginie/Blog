3cbfe0b9a211e964c6ce3dc3b264c4a6f167f7cd
Vivi <vachetvirginie@gmail.com>
2021-03-02 18:35:47 +0100

    add vue

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,9 @@
         </NuxtLink>
       </li>
     </ul>
-    <h3 class="mb-4 font-bold text-2xl uppercase text-center text-pink-800">Topics:</h3>
+    <h3 class="mb-4 font-bold text-2xl uppercase text-center text-pink-800">
+      Topics:
+    </h3>
     <ul class="flex flex-wrap mb-4 text-center">
       <li
         v-for="tag of tags"
